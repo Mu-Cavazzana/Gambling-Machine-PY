e0b2269da7ece876b0c916db57adbe14131cb6b7
Murilo Cavazzana <125220696+Mu-Cavazzana@users.noreply.github.com>
2023-03-23 22:57:31 +0000

    Commit 3 (bugfixes)

--- Main.py
+++ Main.py
@@ -23,18 +23,18 @@ def get_slot_machine_spin(rows, cols, symbols):
         for _ in range(symbol_count):
             all_symbols.append(symbol)
             
-            columns = [[], [], []]
-            for _ in range(cols):
-                column = []
-                current_symbols = all_symbols[:]
-                for _ in range(rows):
-                    value = random.choice(current_symbols)
-                    current_symbols.remove(value)
-                    column.append(value)
+    columns = []
+    for _ in range(cols):
+            column = []
+            current_symbols = all_symbols[:]
+            for _ in range(rows):
+                value = random.choice(current_symbols)
+                current_symbols.remove(value)
+                column.append(value)
                 
-                columns.append(column)
+            columns.append(column)
                 
-            return columns
+    return columns
         
 def print_slot_machine(columns):
     for row in range(len(columns[0])):
@@ -100,8 +100,9 @@ def main():
             break
         
     print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet} ")
-    
-
+   
     slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
     print_slot_machine(slots)
+    
+main()
     
